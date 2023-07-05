25aaccbed7d739b30bdb9e1a8e268cea6d0cf6d4
plon-Susk7 <priyashshah4@gmail.com>
2023-07-06 02:32:06 +0530

    placement initial

--- csrankings/api/urls.py
+++ csrankings/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import overall_ranking
+from .views import *
+
 
 urlpatterns = [
     path('overall_ranking/', overall_ranking, name='overall_ranking'),
+    path('placement/ug/<str:id>', get_placement_ug, name='get_placement_ug'),
 ]
--- csrankings/api/views.py
+++ csrankings/api/views.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -14,5 +15,29 @@ def overall_ranking(request):
 
             # Return the JSON response
             return Response(json_data)
+        
+
+@api_view(['GET'])
+def get_placement_ug(request,id):
+    if request.method == 'GET':
+        # Read the CSV file
+        csv_file_path = f'../csv_files/{id}.csv'
+        if not os.path.isfile(csv_file_path):
+            return Response({'message': 'No data found for the given id'}, status=404)
+
+        with open(csv_file_path, 'r') as csv_file:
+            csv_data = csv.DictReader(csv_file)
+
+            # Convert CSV data to a list of dictionaries
+            json_data = [row for row in csv_data]
+
+            # Return the JSON response
+            return Response(json_data)
+
+@api_view(['GET'])
+def get_placement_pg(request):
+    pass
+
+
         
 
